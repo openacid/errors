89cf861b29ee2e813b2995d2bddfc15a7437b884
drdr xp <drdr.xp@gmail.com>
2019-02-19 14:14:13 +0800

    rename pkg to openacid

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-PKGS := github.com/pkg/errors
+PKGS := github.com/openacid/errors
 SRCDIRS := $(shell go list -f '{{.Dir}}' $(PKGS))
 GO := go
 
--- bench_test.go
+++ bench_test.go
@@ -41,7 +41,7 @@ func BenchmarkErrors(b *testing.B) {
 		{1000, true},
 	}
 	for _, r := range runs {
-		part := "pkg/errors"
+		part := "openacid/errors"
 		if r.std {
 			part = "errors"
 		}
